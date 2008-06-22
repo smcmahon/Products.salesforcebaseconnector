5b4b703d9a275f9cccd5e64c218869c32238b663
Andy Leeb <andy@andersonleeb.com>
2008-06-22 06:44:26 +0000

    merging changes in from trunk r67091

--- Products/salesforcebaseconnector/tests/base.py
+++ Products/salesforcebaseconnector/tests/base.py
@@ -1,25 +1,18 @@
-"""Base class for integration tests, based on ZopeTestCase and PloneTestCase.
+"""Base class for integration tests, based on ZopeTestCase and CMFTestCase.
 
 Note that importing this module has various side-effects: it registers a set of
-products with Zope, and it sets up a sandbox Plone site with the appropriate
+products with Zope, and it sets up a sandbox CMFSite site with the appropriate
 products installed.
 """
 
 from Testing import ZopeTestCase
+from Products.CMFTestCase import CMFTestCase
 
-# Let Zope know about the two products we require above-and-beyond a basic
-# Plone install (PloneTestCase takes care of these).
+# Let Zope know about the two products we require above-and-beyond a basic CMFSite install
 ZopeTestCase.installProduct('salesforcebaseconnector')
 
-# Import PloneTestCase - this registers more products with Zope as a side effect
-from Products.PloneTestCase.PloneTestCase import PloneTestCase
-from Products.PloneTestCase.PloneTestCase import FunctionalTestCase
-from Products.PloneTestCase.PloneTestCase import setupPloneSite
+CMFTestCase.setupCMFSite()
 
-# Set up a Plone site, and apply the membrane and borg extension profiles
-# to make sure they are installed.
-setupPloneSite()
-
-class SalesforceBaseConnectorTestCase(PloneTestCase):
+class SalesforceBaseConnectorTestCase(CMFTestCase.CMFTestCase):
     """Base class for integration tests for the 'salesforcebaseconnector' product.
     """
